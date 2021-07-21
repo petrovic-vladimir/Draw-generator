9478f55b5c3d055dbbd1f3c118ff357262e30ae9
petrovic-vladimir <petrovicphp@gmail.com>
2021-07-21 16:26:29 +0200

    Downloaded files and installed readline-sync node module

---
+++ index.js
@@ -0,0 +1,39 @@
+const readlineSync = require("readline-sync");
+
+const main = () => {
+  const N = readlineSync.question("Unesite broj tenisera (N):");
+
+  const tennisPlayers = [];
+
+  for (var i = 0; i < N; i++) {
+    const tempTennisPlayer = readlineSync.question(
+      "Unesite tenisera u obliku [ime],[prezime],[drzava],[ranking]:"
+    );
+
+    const tempTennisPlayerData = tempTennisPlayer.split(",");
+
+    tennisPlayers.push({
+      firstName: tempTennisPlayerData[0],
+      lastName: tempTennisPlayerData[1],
+      country: tempTennisPlayerData[2],
+      ranking: parseInt(tempTennisPlayerData[3]),
+    });
+  }
+
+  /*
+
+    Your program (assignment) should start here...
+    Variables N and tennisPlayers are available to you.
+
+    Feel free to create new methods, include new files, and change this project as you wish.
+
+    Code above this comment is used to "standardize" input to the application,
+    and should be changed (or built upon) only if you would like to add input validation,
+    or if you have an idea that you would like to demonstrate.
+
+    ...Good Luck and Happy Coding...
+
+  */
+};
+
+main();
